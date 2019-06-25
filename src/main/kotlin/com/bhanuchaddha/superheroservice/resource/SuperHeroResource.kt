f9cc4a6473620d78adf8108cc7002222f009828a
Bhanu Chaddha <bhanuchaddha@gmail.com>
2019-06-25 23:40:14 +0200

    Complete Hero model Hero Service

--- src/main/kotlin/com/bhanuchaddha/superheroservice/resource/SuperHeroResource.kt
+++ src/main/kotlin/com/bhanuchaddha/superheroservice/resource/SuperHeroResource.kt
@@ -2,17 +2,25 @@ package com.bhanuchaddha.superheroservice.resource
 
 import com.bhanuchaddha.superheroservice.integration.superhero.SuperHero
 import com.bhanuchaddha.superheroservice.integration.superhero.SuperHeroApi
+import com.bhanuchaddha.superheroservice.service.SuperHeroService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/superhero")
-class SuperHeroResource(val superHeroApi: SuperHeroApi){
+class SuperHeroResource(val superHeroApi: SuperHeroApi, val superHeroService: SuperHeroService){
 
-    @GetMapping("/{id}")
+
+    @GetMapping("/superhero") //TODO: fix the regex problem
+    fun getAllSuperHeroes(): List<SuperHero>{
+        return superHeroService.getSuperHeroList()!!
+    }
+
+    @GetMapping("/superhero/{id}")
     fun getSuperHero(@PathVariable id: String?): SuperHero{
         return superHeroApi.getSuperHero(id!!).toBlocking().value()
     }
+
+
 }
--- src/main/kotlin/com/bhanuchaddha/superheroservice/service/SuperHeroService.kt
+++ src/main/kotlin/com/bhanuchaddha/superheroservice/service/SuperHeroService.kt
@@ -3,11 +3,23 @@ package com.bhanuchaddha.superheroservice.service
 import com.bhanuchaddha.superheroservice.integration.superhero.SuperHero
 import com.bhanuchaddha.superheroservice.integration.superhero.SuperHeroApi
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class SuperHeroService(val superHeroApi: SuperHeroApi) {
 
     fun getSuperHeroList(): List<SuperHero>? {
-        return null
+        createFile()
+        return emptyList()
+    }
+
+
+    private fun createFile() {
+        val file = File(   javaClass.classLoader.getResource(".").getFile() + "/heroes.json")
+        if (file.createNewFile()) {
+            println("File is created!")
+        } else {
+            println("File already exists.")
+        }
     }
 }
