b95de34eec9216feaf820c9277844dcf355be56b
Bhanu Chaddha <bhanuchaddha@gmail.com>
2019-06-26 21:59:25 +0200

    Json Data created Readme file updated

---
+++ README.md
@@ -0,0 +1,19 @@
+# SuperHero API
+
+
+## Technology
+1. Kotlin
+2. Retrofit
+3. RxJava
+
+## Features
+1. Almost 730 Super hero data available
+
+## Todo
+1. Complete API
+2. Offline Images
+
+
+
+###### Inspiration
+[SuperHero API](https://superheroapi.com/)
--- src/main/kotlin/com/bhanuchaddha/superheroservice/service/SuperHeroService.kt
+++ src/main/kotlin/com/bhanuchaddha/superheroservice/service/SuperHeroService.kt
@@ -2,6 +2,8 @@ package com.bhanuchaddha.superheroservice.service
 
 import com.bhanuchaddha.superheroservice.integration.superhero.SuperHero
 import com.bhanuchaddha.superheroservice.integration.superhero.SuperHeroApi
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.stereotype.Service
 import java.io.File
 
@@ -9,17 +11,34 @@ import java.io.File
 class SuperHeroService(val superHeroApi: SuperHeroApi) {
 
     fun getSuperHeroList(): List<SuperHero>? {
-        createFile()
-        return emptyList()
+        val objectMapper = jacksonObjectMapper()
+        val file = createFile()
+        val superHeroes = mutableListOf<SuperHero>()
+        try {
+            superHeroes.addAll(objectMapper.readValue(file))
+            println("Size of json ${superHeroes.size}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        for (id in 1..732) {
+            val hero = superHeroApi.getSuperHero(id.toString()).toBlocking().value()
+            println(hero)
+
+            superHeroes.add(hero)
+            Thread.sleep(50)
+            objectMapper.writeValue(file, superHeroes)
+        }
+        return superHeroes;
     }
 
 
-    private fun createFile() {
-        val file = File(   javaClass.classLoader.getResource(".").getFile() + "/heroes.json")
+    private fun createFile(): File {
+        val file = File(javaClass.classLoader.getResource(".").getFile() + "/heroes.json")
         if (file.createNewFile()) {
             println("File is created!")
         } else {
             println("File already exists.")
         }
+        return file
     }
 }
